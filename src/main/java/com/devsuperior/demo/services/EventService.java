cf0939a86be2b9935b784f952b3ce8f1b6d39f92
Raphael Vacari <raphaelvacari7@gmail.com>
2024-03-22 21:42:50 -0300

    Event: findAllPaged

--- src/main/java/com/devsuperior/demo/services/EventService.java
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -1,7 +1,25 @@
 package com.devsuperior.demo.services;
 
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.entities.Event;
+import com.devsuperior.demo.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    @Transactional(readOnly = true)
+    public Page<EventDTO> findAll(Pageable pageable) {
+        Page<Event> list = repository.findAll(pageable);
+        return list.map(x -> new EventDTO(x));
+    }
+
+
 }
